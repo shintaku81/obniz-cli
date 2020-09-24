35ce390a7782280312758b1a7f142a31b38110d7
takasago <vipqiv00@gmail.com>
2020-09-24 14:53:46 +0900

    ADD: esptool

---
+++ dist/libs/os/esptool/index.js
@@ -0,0 +1,239 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const bufferpack = __importStar(require("bufferpack"));
+const serialport_1 = __importDefault(require("serialport"));
+const DEFAULT_CONNECT_ATTEMPTS = 7;
+const DEFAULT_TIMEOUT = 3;
+const SYNC_TIMEOUT = 0.1;
+class ESP {
+    constructor(port, baud) {
+        // Default baudrate. The ROM auto-bauds, so we can use more or less whatever we want.
+        this.ESP_ROM_BAUD = 115200;
+        // Commands supported by ESP8266 ROM bootloader
+        this.ESP_SYNC = 0x08;
+        this.ESP_READ_REG = 0x0a;
+        // Response code(s) sent by ROM
+        this.ROM_INVALID_RECV_MSG = 0x05; // response if an invalid message is received
+        // Maximum block sized for RAM and Flash writes, respectively.
+        this.ESP_RAM_BLOCK = 0x1800;
+        this.USB_RAM_BLOCK = 0x800; // Max block size USB CDC is used
+        this.UARTDEV_BUF_NO = 0x3ffffd14; // Variable in ROM .bss which indicates the port in use
+        this.UARTDEV_BUF_NO_USB = 2; // # Value of the above variable indicating that USB is in use
+        this._port = new serialport_1.default(port, { baudRate: baud });
+        this._slip_reader = slip_reader(this._port);
+        this._port.on("open", async () => {
+            await this.connect();
+        });
+    }
+    // connect
+    /*
+     * Try connecting repeatedly until successful, or giving up
+     */
+    async connect(mode = "default_reset", attempts = DEFAULT_CONNECT_ATTEMPTS) {
+        process.stdout.write("Connecting...");
+        let last_error = null;
+        try {
+            for (let i = 0; i < attempts; i++) {
+                last_error = await this._connect_attempt({ mode, esp32r0_delay: false });
+                if (last_error === null) {
+                    break;
+                }
+                last_error = await this._connect_attempt({ mode, esp32r0_delay: true });
+                if (last_error === null) {
+                    break;
+                }
+            }
+        }
+        finally {
+            console.log("");
+        }
+        if (last_error != null) {
+            throw Error(last_error);
+        }
+        this._post_connect();
+    }
+    uses_usb() {
+        const buf_no = this.read_reg(this.UARTDEV_BUF_NO) & 0xff;
+        return buf_no === this.UARTDEV_BUF_NO_USB;
+    }
+    sync() {
+        this.command({
+            op: this.ESP_SYNC,
+            data: [0x07, 0x07, 0x12, 0x20].concat(Array(32).fill(0x55)),
+            timeout: SYNC_TIMEOUT,
+        });
+        for (let i = 0; i < 7; i++) {
+            this.command({});
+        }
+    }
+    command({ op = null, data = [], chk = 0, wait_response = true, timeout = DEFAULT_TIMEOUT }) {
+        // timeout??
+        if (op !== null) {
+            const pkt = Buffer.concat([bufferpack.pack("<BBHI", [0x00, op, data.length, chk]), Buffer.from(data)]);
+            this.write(pkt);
+        }
+        if (!wait_response) {
+            return;
+        }
+        for (let retry = 0; retry < 100; retry++) {
+            const p = this.read();
+            console.log(p);
+            if (p.length < 8) {
+                continue;
+            }
+            const [resp, op_ret, len_ret, val] = bufferpack.unpack("<BBHI", p.slice(0, 8));
+            if (resp !== 1) {
+                continue;
+            }
+            const recv_data = p.slice(8);
+            console.log(recv_data);
+            if (op === null || op_ret === op) {
+                return [val, recv_data];
+            }
+            if (recv_data[0] !== 0 && recv_data[1] === this.ROM_INVALID_RECV_MSG) {
+                throw Error("Unsupported command.");
+            }
+        }
+        throw Error("Response doesn't match request");
+    }
+    async write(packet) {
+        // replace?
+        const buf = Buffer.concat([Buffer.from([0xc0]), packet, Buffer.from([0xc0])]);
+        console.log(buf);
+        this._port.write(buf, "ascii");
+        this._port.drain();
+    }
+    read() {
+        const result = this._slip_reader.next();
+        return result.value;
+    }
+    flush_input() {
+        this._slip_reader = slip_reader(this._port);
+    }
+    read_reg(addr) {
+        const [val, data] = this.command({
+            op: this.ESP_READ_REG,
+            data: bufferpack.pack("<I", addr),
+        });
+        if (data[0] !== 0) {
+            throw Error(`Failed to read register address ${addr}`);
+        }
+        return val;
+    }
+    _post_connect() {
+        if (this.uses_usb()) {
+            this.ESP_RAM_BLOCK = this.USB_RAM_BLOCK;
+        }
+    }
+    async _connect_attempt({ mode = "default_reset", esp32r0_delay = false }) {
+        let last_error = null;
+        if (mode !== "no_reset") {
+            this._setDTR(false); // IO0=HIGH
+            this._setRTS(true); // EN=LOW, chip in reset
+            await this._sleep(100);
+            if (esp32r0_delay) {
+                await this._sleep(1200);
+            }
+            this._setDTR(true); // IO0=LOW
+            this._setRTS(false); // EN=HIGH, chip out of reset
+            if (esp32r0_delay) {
+                await this._sleep(400);
+            }
+            await this._sleep(50);
+            this._setDTR(false); // IO0=HIGH, done
+        }
+        for (let i = 0; i < 5; i++) {
+            try {
+                this.flush_input();
+                this._port.flush();
+                this.sync();
+                return null;
+            }
+            catch (err) {
+                console.log(err);
+                if (esp32r0_delay) {
+                    process.stdout.write("_");
+                }
+                else {
+                    process.stdout.write(".");
+                }
+                await this._sleep(50);
+                last_error = err;
+            }
+        }
+        return last_error;
+    }
+    // get chip info
+    // stub?
+    // change baud rate
+    // send command
+    _setDTR(state) {
+        this._port.set({ dtr: state });
+    }
+    _setRTS(state) {
+        this._port.set({ rts: state });
+    }
+    _sleep(msec) {
+        return new Promise((resolve) => setTimeout(resolve, msec));
+    }
+}
+exports.default = ESP;
+function* slip_reader(port) {
+    let partial_packet = null;
+    let in_escape = false;
+    while (true) {
+        const read_bytes = port.read();
+        const waiting_for = partial_packet === null ? "header" : "content";
+        if (read_bytes === null) {
+            throw Error(`Timed out waiting for packet ${waiting_for}`);
+        }
+        console.log(read_bytes.toString("hex"));
+        for (const b of read_bytes) {
+            if (partial_packet === null) {
+                // waiting for packet header
+                if (b === 0xc0) {
+                    // header
+                    partial_packet = Buffer.from([]);
+                }
+                else {
+                    throw Error(`Invalid head of packet (${Number(b).toString(16)})`);
+                }
+            }
+            else if (in_escape) {
+                in_escape = false;
+                if (b === 0xdc) {
+                    partial_packet = Buffer.concat([partial_packet, 0xc0]);
+                }
+                else if (b === 0xdd) {
+                    partial_packet = Buffer.concat([partial_packet, 0xdb]);
+                }
+                else {
+                    throw Error(`Invalid SLIP escape (0xdb, 0x${Number(b).toString(16)})`);
+                }
+            }
+            else if (b === 0xdb) {
+                // start of escape sequence
+                in_escape = true;
+            }
+            else if (b === 0xc0) {
+                // end of packet
+                yield partial_packet;
+                partial_packet = null;
+            }
+            else {
+                // normal byte in packet
+                partial_packet = Buffer.concat([partial_packet, b]);
+            }
+        }
+    }
+}
--- src/libs/os/erase.ts
+++ src/libs/os/erase.ts
@@ -1,31 +1,34 @@
 import child_process from "child_process";
 
-export default (obj: { portname: string; baud: number; stdout: any }) => {
-  return new Promise((resolve, reject) => {
-    let received = "";
-    let success = false;
-    obj.stdout("", { clear: true });
-    const cmd = `esptool.py --chip esp32 --port ${obj.portname} --baud ${obj.baud} erase_flash`;
-    console.log(cmd);
-    const child = child_process.exec(cmd);
-    child.stdout.setEncoding("utf8");
-    child.stdout.on("data", (text) => {
-      obj.stdout(text);
-      received += text;
-      if (received.indexOf("Chip erase completed successfully") >= 0) {
-        // 終わったっぽい
-        success = true;
-      }
-    });
-    child.stderr.on("data", (text) => {
-      obj.stdout(text);
-      received += text;
-    });
-    child.on("error", (err) => {
-      reject(err);
-    });
-    child.on("exit", () => {
-      resolve(success);
-    });
-  });
+import ESP from "./esptool";
+
+export default async (obj: { portname: string; baud: number; stdout: any }) => {
+  const esp = new ESP(obj.portname, obj.baud);
+  // return new Promise((resolve, reject) => {
+  //   let received = "";
+  //   let success = false;
+  //   obj.stdout("", { clear: true });
+  //   const cmd = `esptool.py --chip esp32 --port ${obj.portname} --baud ${obj.baud} erase_flash`;
+  //   console.log(cmd);
+  //   const child = child_process.exec(cmd);
+  //   child.stdout.setEncoding("utf8");
+  //   child.stdout.on("data", (text) => {
+  //     obj.stdout(text);
+  //     received += text;
+  //     if (received.indexOf("Chip erase completed successfully") >= 0) {
+  //       // 終わったっぽい
+  //       success = true;
+  //     }
+  //   });
+  //   child.stderr.on("data", (text) => {
+  //     obj.stdout(text);
+  //     received += text;
+  //   });
+  //   child.on("error", (err) => {
+  //     reject(err);
+  //   });
+  //   child.on("exit", () => {
+  //     resolve(success);
+  //   });
+  // });
 };
