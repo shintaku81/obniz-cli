8661914f8d80f8cf5eb1e4762af5f8cafe1ad942
masudayasuto <masuda@ciebus.net>
2024-02-26 16:44:09 +0900

    [wip] revert esptool.js

--- src/libs/os/erase.ts
+++ src/libs/os/erase.ts
@@ -1,39 +1,31 @@
-import { EspLoader } from "@9wick/esptool.js";
-import { EsptoolSerial } from "@9wick/esptool.js/build/node/serial";
 import child_process from "child_process";
 
-export default async (obj: { portname: string; baud: number; stdout: any }) => {
-  obj.stdout("", { clear: true });
-
-  const port = new EsptoolSerial(obj.portname, {
-    baudRate: 115200,
-    autoOpen: false,
+export default (obj: { portname: string; baud: number; stdout: any }) => {
+  return new Promise((resolve, reject) => {
+    let received = "";
+    let success = false;
+    obj.stdout("", { clear: true });
+    const cmd = `esptool.py --chip esp32 --port ${obj.portname} --baud ${obj.baud} erase_flash`;
+    console.log(cmd);
+    const child = child_process.exec(cmd);
+    child.stdout?.setEncoding("utf8");
+    child.stdout?.on("data", (text) => {
+      obj.stdout(text);
+      received += text;
+      if (received.indexOf("Chip erase completed successfully") >= 0) {
+        // 終わったっぽい
+        success = true;
+      }
+    });
+    child.stderr?.on("data", (text) => {
+      obj.stdout(text);
+      received += text;
+    });
+    child.on("error", (err) => {
+      reject(err);
+    });
+    child.on("exit", () => {
+      resolve(success);
+    });
   });
-  await port.open();
-
-  const espTool = new EspLoader(port, {
-    debug: false,
-    logger: {
-      log(message?: unknown, ...optionalParams) {},
-      debug(message?: unknown, ...optionalParams) {},
-      error(message?: unknown, ...optionalParams) {},
-    },
-  });
-
-  await espTool.connect();
-
-  const chipName = await espTool.chipName();
-  const macAddr = await espTool.macAddr();
-  console.log("chipName", chipName);
-  console.log("macAddr", macAddr);
-  await espTool.loadStub();
-  if (obj.baud) {
-    const baud = typeof obj.baud === "number" ? obj.baud : parseInt(obj.baud);
-    console.log(obj, baud);
-    await espTool.setBaudRate(115200, baud);
-  }
-  console.log("Start erase");
-  await espTool.eraseFlash();
-  console.log("Chip erase completed successfully");
-  await espTool.disconnect();
 };
